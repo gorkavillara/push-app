595c3e8e8361a4698620f185123fb9dec70b52e9
Gorka <gorkavillara@gmail.com>
2022-10-09 21:29:43 +0200

    📞 Mensajes push mejorados

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,6 @@
 self.addEventListener("install", () => self.skipWaiting())
 
-self.addEventListener("push", (e) => console.log(e))
+self.addEventListener("push", (event) => {
+    const { title, message, icon } = event.data.json();
+    self.registration.showNotification(title, { body: message, icon });
+});
